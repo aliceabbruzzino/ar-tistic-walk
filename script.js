b8163ac2a23d68496b39ec6582f537fb1b2cb868
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2022-03-03 15:58:29 +0000

    Try with JS

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-window.onload = () => {
-    // setTimeout(() => {
-    let places = staticLoadPlaces();
-    return renderPlaces(places);
-    // }, 3000)
-
-    function staticLoadPlaces() {
-      return [{
-        name: "CSIS",
-        location: {
-          lat: 52.674083, // change here latitude if using static data
-          lng: -8.575815, // change here longitude if using static data
-        }
-      }];
-    }
-
+function getLocation() {
+  if (navigator.geolocation) {
     function renderPlaces(places) {
       let scene = document.querySelector('a-scene');
 
@@ -23,7 +9,7 @@ window.onload = () => {
 
         // add place name
         let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gps-entity-place', `latitude: ${52.674083}; longitude: ${-8.575815};`);
         model.setAttribute('gltf-model', 'assets/CSIS_building/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
@@ -36,4 +22,30 @@ window.onload = () => {
         scene.appendChild(model);
       });
     }
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
+}
+    function renderPlaces(places) {
+      let scene = document.querySelector('a-scene');
+
+      places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        // add place name
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${52.674083}; longitude: ${-8.575815};`);
+        model.setAttribute('gltf-model', 'assets/CSIS_building/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '50 50 50');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+      });
+    }
+  
