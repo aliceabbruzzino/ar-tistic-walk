c884b46080ced154b3c84dd76fcba508f3e95540
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2022-03-03 16:43:43 +0000

    Trying with Hikar thing

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
+let firstLoc = true;
+
+window.onload = () => {
+    const camera = document.querySelector('a-camera');
+    window.addEventListener("gps-camera-update-position", e => {
+        if(firstLoc) {
+			firstLoc = false;
+            alert(`Got GPS: you are at: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
+            setPos(e.detail.position.longitude, e.detail.position.latitude);
+        }
+    });    
 };
 
-function success(pos) {
-  var crd = pos.coords;
-
-  console.log('Your current position is:');
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
+function setPos(longitude,latitude) {
+        const box = document.createElement("a-box");
+        box.setAttribute('scale', {
+            x: 10, 
+            y: 10,
+            z: 10
+        });
+        box.setAttribute('material', {
+            color: 'red'
+        });
+        box.setAttribute('gps-projected-entity-place', {
+            latitude: latitude+0.0006,
+            longitude: longitude-0.0005
+        });
+        const box2 = document.createElement("a-box");
+        box2.setAttribute('scale', {
+            x: 10, 
+            y: 10,
+            z: 10
+        });
+        box2.setAttribute('material', {
+            color: 'yellow'
+        });
+        box2.setAttribute('gps-projected-entity-place', {
+            latitude: latitude+0.0004,
+            longitude: longitude-0.0001
+        });
+        const sceneEl = document.querySelector("a-scene");
+        sceneEl.appendChild(box);
+        sceneEl.appendChild(box2);
 }
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
-
-const watchID = navigator.geolocation.watchPosition((position) => {
-  doSomething(position.coords.latitude, position.coords.longitude);
-});
