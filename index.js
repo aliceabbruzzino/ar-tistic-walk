ad99ab9b9b9de7d26c3f43c0d9ac25cc9df5335e
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2022-03-12 15:28:19 +0000

    upload

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
+
 var id, target, options;
 
 function success(pos) {
   var crd = pos.coords;
   var ARobjects = [
-    "<a-entity gltf-model='#Living_bridge' gps-entity-place='latitude:52.676577; longitude:-8.570377;' scale='50 50 50' animation-mixer animation-loop> </a-entity>",
+    "<a-entity gltf-model='#Living_bridge' gps-entity-place='latitude:52.679; longitude:-8.577;' scale='50 50 50' animation-mixer animation-loop> </a-entity>",
     "<a-entity gltf-model='#Library' gps-entity-place='latitude:52.673542; longitude:-8.572933;' scale='50 50 50' animation-mixer animation-loop> </a-entity>",
     "<a-entity gltf-model='#HS_building' gps-entity-place='latitude:52.678524; longitude:-8.568776;' scale='50 50 50' animation-mixer animation-loop> </a-entity>",
     "<a-entity gltf-model='#Foundation_building' gps-entity-place='latitude:52.674187; longitude:-8.573059;' scale='50 50 50' animation-mixer animation-loop> </a-entity>",
     "<a-entity gltf-model='#CSIS_building' gps-entity-place='latitude:52.674083; longitude:-8.575815;' scale='50 50 50' animation-mixer animation-loop> </a-entity>"
   ]
 
-  ARobjects.forEach((htmlString, index) => {
-    if(target[index].latitude === crd.latitude && target[index].longitude === crd.longitude){
+  // living bridge real latitude:52.676577; longitude:-8.570377;
 
-    document.getElementById("livingBridge").outerHTML = htmlString;
-    document.getElementById("library").outerHTML = htmlString;
-    document.getElementById("hsBuilding").outerHTML = htmlString;
-    document.getElementById("foundationBuilding").outerHTML = htmlString;
-    document.getElementById("csisBuilding").outerHTML = htmlString;
+  ARobjects.forEach((htmlString, index) => {
+    if (target[index].latitude === crd.latitude && target[index].longitude === crd.longitude) {
+
+      document.getElementById("renderer").outerHTML = htmlString;
 
     }
   })
@@ -28,22 +27,19 @@ function error(err) {
 }
 
 target = [
-  {latitude: 52.676577, longitude: -8.570377},
-  {latitude:52.673542, longitude:-8.572933},
-  {latitude:52.678524, longitude:-8.568776},
-  {latitude:52.674187, longitude:-8.573059},
-  {latitude:52.674083, longitude:-8.575815}];
+  { latitude: 52.679, longitude: -8.577 },
+  { latitude: 52.673542, longitude: -8.572933 },
+  { latitude: 52.678524, longitude: -8.568776 },
+  { latitude: 52.674187, longitude: -8.573059 },
+  { latitude: 52.674083, longitude: -8.575815 }];
 
 options = {
-  enableHighAccuracy: false,
+  enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
 id = navigator.geolocation.watchPosition(success, error, options);
-
-
-
 
 
 
