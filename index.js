c30a182008fc8958109ccb9d5393880e4f66857d
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2022-03-16 11:07:43 +0000

    update
    projected thing

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var id, target, options;
 function success(pos) {
   var crd = pos.coords;
   var ARObjects = [
-    "<a-entity id='renderer' gltf-model='#Living_bridge' gps-entity-place='latitude: 52.679236; longitude: -8.577019;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#Library' gps-entity-place='latitude:52.673542; longitude:-8.572933;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#HS_building' gps-entity-place='latitude:52.678524; longitude:-8.568776;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#Foundation_building' gps-entity-place='latitude:52.674187; longitude:-8.573059;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#CSIS_building' gps-entity-place='latitude:52.674083; longitude:-8.575815;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>"
+    "<a-entity id='renderer' gltf-model='#Living_bridge' gps-projected-entity-place='latitude: 52.679236; longitude: -8.577019;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
+    "<a-entity id='renderer' gltf-model='#Library' projected-='latitude:52.673542; longitude:-8.572933;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
+    "<a-entity id='renderer' gltf-model='#HS_building' projected-='latitude:52.678524; longitude:-8.568776;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
+    "<a-entity id='renderer' gltf-model='#Foundation_building' projected-='latitude:52.674187; longitude:-8.573059;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
+    "<a-entity id='renderer' gltf-model='#CSIS_building' projected-='latitude:52.674083; longitude:-8.575815;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>"
   ]
 
   ARObjects.forEach((htmlString, index) => {
