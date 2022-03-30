5893411123127fec1ca655cd80c0d6f29acc3bc5
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2022-03-30 20:21:56 +0100

    update
    trying to combine markers with index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var id, target, options;
 function success(pos) {
   var crd = pos.coords;
   var ARObjects = [
-    "<a-entity id='renderer' gltf-model='#Living_bridge' gps-projected-entity-place='latitude: 52.679236; longitude: -8.577019;' scale='50 50 50' position='0 0 -200' rotation='0 180 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#Library' projected-='latitude:52.673542; longitude:-8.572933;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#HS_building' projected-='latitude:52.678524; longitude:-8.568776;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#Foundation_building' projected-='latitude:52.674187; longitude:-8.573059;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>",
-    "<a-entity id='renderer' gltf-model='#CSIS_building' projected-='latitude:52.674083; longitude:-8.575815;' scale='50 50 50' position='10 30 0' animation-mixer animation-loop> </a-entity>"
-  ]
+    "<a-entity id='rederer' position='0 0 0' scale='0.5 0.5 0.5' gltf-model='#livingBridge' animation-mixer></a-entity>",
+    "<a-entity id='rederer' position='0 0 0' scale='0.5 0.5 0.5' gltf-model='#library' animation-mixer></a-entity>",
+    "<a-entity id='rederer' position='0 0 0' scale='0.5 0.5 0.5' gltf-model='#hsBuilding' animation-mixer></a-entity>",
+    "<a-entity id='rederer' position='0 0 0' scale='0.5 0.5 0.5' gltf-model='#csisBuilding' animation-mixer></a-entity>",
+    "<a-entity id='rederer' position='0 0 0' scale='0.5 0.5 0.5' gltf-model='#foundationBuilding' animation-mixer></a-entity>",
+    "<a-entity id='rederer' position='0 0 0' scale='0.5 0.5 0.5' gltf-model='#rsBuilding' animation-mixer></a-entity>"]
 
   ARObjects.forEach((htmlString, index) => {
     setTimeout(() => {
