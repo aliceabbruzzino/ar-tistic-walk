fe5015f6aaa4ec96a68caba55aaa5be2929b0296
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2022-04-01 13:41:03 +0100

    update
    added qr code in the beginning page

--- general.js
+++ general.js
@@ -9,6 +9,9 @@ function changeImage() {
         image.src = "./icons/begin_eye.svg";
         document.getElementById("caption").innerHTML="<h3>Viewing</h3><p>Once you've reached the near location, <br> open the view page from the menu</p>";
     } else if (image.getAttribute('src') == "./icons/begin_eye.svg") {
+        image.src = "./icons/begin_qrcode.svg";
+        document.getElementById("caption").innerHTML="<h3>Scanning</h3><p>Hold the QR code in your hand <br> and let the prjection happen</p>";
+    } else if (image.getAttribute('src') == "./icons"){
         image.src = "./icons/begin_rocket.svg";
         document.getElementById("caption").innerHTML="<h3>Departure</h3><p>You are now ready to start seeing things <br> from another point of view</p>";
     } else {
